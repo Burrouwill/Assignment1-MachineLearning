220fc500a2f3b6726a4216f0b8abaf83506ec6c8
Burrouwill <wgr.burroughs@gmail.com>
2023-07-29 08:01:42 +1200

    Draws a terrible box plot for knn

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,7 +1,9 @@
+import warnings
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
@@ -20,55 +22,73 @@ datasets = {
 # Define classifiers and datasets
 classifiers = {
     'KNN': KNeighborsClassifier(),
-    'GNB': GaussianNB(),
-    'DT': DecisionTreeClassifier(),
-    'LR': LogisticRegression(),
-    'GB': GradientBoostingClassifier(),
-    'RF': RandomForestClassifier(),
-    'MLP': MLPClassifier()
 }
 
 # Define a dictionary to hold the results
 results = {}
 
-# Define parameter values for each classifier
+# Define hyperparameter values for each classifier
 parameter_values = {
-    'KNN': {'n_neighbors': [3, 5, 7]},
-    'GNB': {},  # GaussianNB has no hyperparameters
-    'DT': {'max_depth': [None, 5, 10]},
-    'LR': {'C': [0.1, 1.0, 10.0]},
-    'GB': {'n_estimators': [50, 100, 150]},
-    'RF': {'n_estimators': [50, 100, 150]},
-    'MLP': {'hidden_layer_sizes': [(100,), (50, 50), (30, 30, 30)]}
+    'KNN': {'n_neighbors': [3, 5, 7]}
 }
 
-# Loop through classifiers and datasets to evaluate accuracy with different parameter values
-for clf_name, clf in classifiers.items():
-    clf_results = []
-    for dataset_name, dataset in datasets.items():
-        X, y = dataset.data, dataset.target
-        if sparse.issparse(X):
-            X = X.toarray()  # Convert sparse matrix to dense array for some classifiers
-        clf_scores = []
-        if clf_name in parameter_values:
-            for param_name, param_values in parameter_values[clf_name].items():
-                clf_scores.extend(cross_val_score(clf, X, y, cv=3, param_name=param_name, param_values=param_values))
-        else:
-            scores = cross_val_score(clf, X, y, cv=3)
-            clf_scores.append(scores)
-        clf_results.append(clf_scores)
-    results[clf_name] = clf_results
+# Extract features and labels for each dataset
+X_steel, y_steel = datasets["steel-plates-fault"]["data"], datasets["steel-plates-fault"]["target"]
+X_ionosphere, y_ionosphere = datasets["ionosphere"]["data"], datasets["ionosphere"]["target"]
+X_banknotes, y_banknotes = datasets["banknotes"]["data"], datasets["banknotes"]["target"]
 
-# Plot the results as boxplots
-fig, axes = plt.subplots(nrows=len(classifiers), ncols=len(datasets), figsize=(15, 20))
-for i, clf_name in enumerate(classifiers.keys()):
-    for j, dataset_name in enumerate(datasets.keys()):
-        ax = axes[i][j]
-        param_names = list(parameter_values[clf_name].keys()) if clf_name in parameter_values else ['Default']
-        ax.boxplot(results[clf_name][j], labels=param_names)
-        ax.set_title(f'{clf_name} - {dataset_name}')
-        ax.set_ylabel('Accuracy')
-        ax.set_xlabel('Parameter Values')
+
+def knn_hyperparameter_tuning(X, y, k_values=[1, 2, 3, 4, 5], num_splits=50, random_seed=42):
+    test_accuracies = []
+
+    for _ in range(num_splits):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=random_seed)
+
+        # Create and train the KNeighborsClassifier with different hyperparameter settings
+        for k in k_values:
+            knn = KNeighborsClassifier(n_neighbors=k)
+            knn.fit(X_train, y_train)
+
+            # Evaluate the model on the test set
+            accuracy = knn.score(X_test, y_test)
+            test_accuracies.append((k, accuracy))
+
+    return test_accuracies
+
+
+# Perform hyperparameter tuning for each dataset
+steel_accuracies = knn_hyperparameter_tuning(X_steel, y_steel)
+ionosphere_accuracies = knn_hyperparameter_tuning(X_ionosphere, y_ionosphere)
+banknotes_accuracies = knn_hyperparameter_tuning(X_banknotes, y_banknotes)
+
+
+def plot_boxplot(dataset_accuracies, dataset_name):
+    plt.figure(figsize=(10, 6))
+    k_values = [entry[0] for entry in dataset_accuracies]
+    accuracies = [entry[1] for entry in dataset_accuracies]
+    plt.boxplot(accuracies)
+    plt.xticks(np.arange(len(k_values)) + 1, k_values)
+    plt.xlabel("Number of Neighbors (k)")
+    plt.ylabel("Classification Accuracy")
+    plt.title(f"KNeighborsClassifier Hyperparameter Tuning for {dataset_name}")
+    plt.show()
+
+# Create a table with separate main plots for each dataset
+fig, axs = plt.subplots(3, 1, figsize=(8, 20), sharex=True)
+
+datasets_names = ["steel-plates-fault", "ionosphere", "banknotes"]
+datasets_accuracies = [steel_accuracies, ionosphere_accuracies, banknotes_accuracies]
+k_values = list(range(1, 21))  # k values from 1 to 20
+
+for i, dataset_name in enumerate(datasets_names):
+    dataset_accuracies = datasets_accuracies[i]
+    accuracies = [entry[1] for entry in dataset_accuracies]
+    axs[i].boxplot(accuracies)
+    axs[i].set_xticks(np.arange(len(k_values)) + 1)
+    axs[i].set_xticklabels(k_values)
+    axs[i].set_xlabel("Number of Neighbors (k)")
+    axs[i].set_ylabel("Classification Accuracy")
+    axs[i].set_title(f"{dataset_name} - KNeighborsClassifier")
 
 plt.tight_layout()
 plt.show()
